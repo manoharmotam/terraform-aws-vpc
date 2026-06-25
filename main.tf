1bc1d907e38554f5ff7e07b3fa798710c38faa5e
manoharmotam <b3stsinceday1@gmail.com>
2026-06-25 08:52:27 +0000

    Update: terraform VPC example

--- main.tf
+++ main.tf
@@ -71,5 +71,29 @@ resource "aws_route_table_association" "database" {
 
 resource "aws_eip" "nat" {
     domain = "vpc"
-    
+}
+
+resource "aws_nat_gateway" "nat" {
+    allocation_id = aws_eip.nat.id
+    subnet_id = aws_subnet.public_subnet[0].id
+
+    depends_on = [ aws_internet_gateway.main ]
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route" "private" {
+  route_table_id = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_nat_gateway.nat.id
+}
+
+resource "aws_route" "database" {
+  route_table_id = aws_route_table.database.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id = aws_internet_gateway.main.id
 }
