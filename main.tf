ab8c0018899d7278f91c026499e797ca783c642c
manoharmotam <b3stsinceday1@gmail.com>
2026-06-25 08:42:37 +0000

    Update: terraform VPC example

--- main.tf
+++ main.tf
@@ -38,3 +38,38 @@ resource "aws_subnet" "database_subnet" {
   availability_zone = local.azs[count.index]
 #   tags = merge(local.common_name,local.common_tags)
 }
+
+resource "aws_route_table" "public" {
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "database" {
+    vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr)
+  subnet_id = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  subnet_id = aws_subnet.private_subnet[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidr)
+  subnet_id = aws_subnet.database_subnet[count.index].id
+  route_table_id = aws_route_table.database.id
+}
+
+resource "aws_eip" "nat" {
+    domain = "vpc"
+    
+}
